1d06ece0f1a8ae62bdf3045c7491f8dc6bffe1f2
Anshudhar <anshudhar2001@gmail.com>
2021-04-28 12:50:33 +0530

    chat ui

--- components/Home/Chat/Chat.js
+++ components/Home/Chat/Chat.js
@@ -1,6 +1,6 @@
 import { useRoute } from '@react-navigation/core'
 import React, { useEffect, useState } from 'react'
-import { View, Text, TextInput, Button } from 'react-native'
+import { View, Text, TextInput, Button, StyleSheet, Dimensions, ScrollView } from 'react-native'
 import { useSelector } from 'react-redux'
 import { addMessage } from '../../../firebase/functions'
 import { subChat } from '../../../firebase/subscriptions'
@@ -48,16 +48,20 @@ export default function Chat() {
     }
 
     return (
-        <View>
-            <Text>{route.params.uid}</Text>
-            {chatList.map((item) => {
-                return (
-                    <ChatItem data={item} key={item.ccid} />
-                )
-            })}
-            <TextInput placeholder="Type Your Message Here"
+        <View style={{ flex: 1 }}>
+            <ScrollView>
+                <Text style={{ alignSelf: 'center' }}>{route.params.uid}</Text>
+                {chatList.map((item) => {
+                    return (
+                        <ChatItem data={item} key={item.ccid} />
+                    )
+                })}
+            </ScrollView>
+            <TextInput style={styles.sendInput} multiline={true} placeholder="Type Your Message Here"
                 value={message}
                 onChangeText={(mess) => setMessage(mess)}
+                maxLength={30}
+                numberOfLines={1}
             />
             {message.length > 0 ? <Button onPress={sendMessage} title="Send Message" /> : null}
         </View>
@@ -68,9 +72,12 @@ const ChatItem = ({ data }) => {
     const allUser = useSelector(selectAllUser);
     const user = useSelector(selectUser)
     const time = new Date(data.time)
+
+    const alignDirection = data.uid == user.uid ? 'flex-start' : 'flex-end';
+
     if (!data) {
         return (
-            <View>
+            <View style={[styles.chatItem, { alignSelf: alignDirection }]}>
                 <Text>Error in Chat</Text>
             </View>
         )
@@ -78,13 +85,13 @@ const ChatItem = ({ data }) => {
 
     if (data.type == 'post') {
         return (
-            <View>
+            <View style={[styles.chatItem, { alignSelf: alignDirection }]}>
                 <Text>Post with Pid: {data.content}</Text>
             </View>
         )
     }
     return (
-        <View>
+        <View style={[styles.chatItem, { alignSelf: alignDirection }]}>
             <Text>{data.content}</Text>
             <Text>Sender: {data.uid == user.uid ? "ME" : allUser[data.uid].name} at {time.toLocaleString()}</Text>
         </View>
@@ -113,3 +120,25 @@ function getChatList(data) {
     list.sort((a, b) => a.time > b.time)
     return list
 }
+
+const styles = StyleSheet.create({
+    chatItem: {
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        backgroundColor: 'green',
+        padding: 10,
+        width: Dimensions.get('window').width / 1.5,
+        margin: 5,
+        borderRadius: 10
+    },
+    sendInput: {
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        borderRadius: 5,
+        padding: 10,
+        margin: 5,
+        borderWidth: 1
+    }
+})
